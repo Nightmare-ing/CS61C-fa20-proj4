6412d1ca2ff68ddea6da990a5a75271518b5dd4e
Night <kingsanhu@gmail.com>
2023-10-26 10:47:36 +0800

    Finish Task2
    Write setup.py for building python extensions from C

--- setup.py
+++ setup.py
@@ -6,7 +6,14 @@ def main():
     LDFLAGS = ['-fopenmp']
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
-    # TODO: YOUR CODE HERE
+    numc_module = Extension('numc',
+                            extra_compile_args=CFLAGS,
+                            sources=['numc.c'],
+                            extra_link_args=LDFLAGS)
+
+    setup(name='numcPackage',
+          version='1.0',
+          ext_modules=[numc_module])
 
 if __name__ == "__main__":
     main()
